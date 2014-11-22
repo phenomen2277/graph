bb426bbbc303a53c14b7416cc435007782d4f0df
Jone Samra <jonemob@gmail.com>
2014-11-22 22:42:19 +0100

    updated

--- topological_sort.rb
+++ topological_sort.rb
@@ -9,21 +9,21 @@ class TopologicalSort
 	end
 
 	def topological_sort(graph, start_node)
+		raise TypeError unless graph.class.ancestors.include?(Graph)
 		raise TypeError unless start_node.class.ancestors.include?(Node)
 		raise RuntimeError, "The graph is cyclic" if GraphUtils.is_cyclic(graph)
+		node = graph.get_node_by_name(start_node.name)
+		raise RuntimeError if node == nil
 
 		@visisted_nodes.clear if @visisted_nodes.size > 0
 		@list.clear if @list.size > 0
 
-		recursive_topological_sort(graph, start_node)
+		recursive_topological_sort(graph, node)
 		@list
 	end
 
 	private
 	def recursive_topological_sort(graph, start_node)
-		node = graph.nodes[start_node.name]
-		raise RutimeError if  start_node == nil
-		
 		@visisted_nodes.push(node)
 		start_node.successors.each do |n|
 			recursive_topological_sort(graph, n) if @visisted_nodes.include?(n) == false
