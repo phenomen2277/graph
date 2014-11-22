99ef569b4255f88d784cf118f79f13bf2103fed6
Jone Samra <jonemob@gmail.com>
2014-11-22 22:42:12 +0100

    updated

--- post_order.rb
+++ post_order.rb
@@ -8,25 +8,25 @@ class PostOrder
 	end
 
 	def post_order(graph, start_node)
+		raise TypeError unless graph.class.ancestors.include?(Graph)
 		raise TypeError unless start_node.class.ancestors.include?(Node)
-		
+		node = graph.get_node_by_name(start_node.name)
+		raise RuntimeError if node == nil
+
 		@visisted_nodes.clear if @visisted_nodes.size > 0
 		@list.clear if @list.size > 0
 
-		recursive_post_order(graph, start_node)
+		recursive_post_order(graph, node)
+		return @list
 	end
 
 	private 
 	def recursive_post_order(graph, start_node)
-		node = graph.nodes[start_node.name]
-		raise RutimeError if  start_node == nil
-		
 		@visisted_nodes.push(node)
 		start_node.successors.each do |n|
 			recursive_post_order(graph, n) if @visisted_nodes.include?(n) == false
 		end
 
 		@list.push(start_node)
-		@list
 	end
 end
